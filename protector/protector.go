4ec76ac863c60196b8ed10e604f4d9c7fb05092b
Ville Valkonen <weezelding@gmail.com>
2022-02-27 14:24:44 +0200

    Remove redundant return

--- protector/protector.go
+++ protector/protector.go
@@ -1,3 +1,4 @@
+//go:build !openbsd
 // +build !openbsd
 
 package protector
@@ -5,6 +6,4 @@ package protector
 // Source https://github.com/junegunn/fzf/blob/a1bcdc225e1c9b890214fcea3d19d85226fc552a/src/protector/protector.go
 
 // Protect calls OS specific protections like pledge on OpenBSD
-func Protect(doesntMatter string) {
-	return
-}
+func Protect(doesntMatter string) {}
