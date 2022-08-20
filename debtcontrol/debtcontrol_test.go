90a31f286e1626c8948fecace5e74a6675eedf5c
Ville Valkonen <weezelding@gmail.com>
2022-08-20 22:49:30 +0300

    Simplify GetSalaryCompensatedDebts function.
    When salary compensated debt is needed and order must be kept in the
    original data structure, this is achieved with pointer parameters. By
    utilizing the slice inside the function we can order the items as we
    like and modify then as needed since they are passed as references.
    Otherwise code would become a dirty if-else jungle.

---
+++ debtcontrol/debtcontrol_test.go
@@ -0,0 +1,143 @@
+package debtcontrol
+
+import (
+	"context"
+	"math"
+	"testing"
+	"weezel/budget/db"
+)
+
+const (
+	floatDelta = float64(1e-4)
+)
+
+func TestGetSalaryCompensatedDebts(t *testing.T) {
+	type args struct {
+		ctx   context.Context
+		user1 *db.StatisticsAggrByTimespanRow
+		user2 *db.StatisticsAggrByTimespanRow
+	}
+	tests := []struct {
+		name        string
+		args        args
+		wantErr     bool
+		updatedArgs map[string]float64 // Args get updated after the fn call
+	}{
+		{
+			name: "Person with smaller salary has no purchases",
+			args: args{
+				ctx: nil,
+				user1: &db.StatisticsAggrByTimespanRow{
+					Username:    "alice",
+					ExpensesSum: 0,
+					Salary:      900.0,
+					Owes:        0,
+				},
+				user2: &db.StatisticsAggrByTimespanRow{
+					Username:    "tom",
+					ExpensesSum: 80.0,
+					Salary:      1000.0,
+					Owes:        0,
+				},
+			},
+			wantErr: false,
+			updatedArgs: map[string]float64{
+				"alice": 37.8947,
+				"tom":   0.0,
+			},
+		},
+		{
+			name: "Person with greater salary has no purchases",
+			args: args{
+				ctx: nil,
+				user1: &db.StatisticsAggrByTimespanRow{
+					Username:    "alice",
+					ExpensesSum: 80.0,
+					Salary:      900.0,
+					Owes:        0,
+				},
+				user2: &db.StatisticsAggrByTimespanRow{
+					Username:    "tom",
+					ExpensesSum: 0,
+					Salary:      1000.0,
+					Owes:        0,
+				},
+			},
+			wantErr: false,
+			updatedArgs: map[string]float64{
+				"alice": 0.0,
+				"tom":   42.1052,
+			},
+		},
+		{
+			name: "Person with smaller salary has more purchases",
+			args: args{
+				ctx: nil,
+				user1: &db.StatisticsAggrByTimespanRow{
+					Username:    "alice",
+					ExpensesSum: 40.0,
+					Salary:      1000.0,
+					Owes:        0,
+				},
+				user2: &db.StatisticsAggrByTimespanRow{
+					Username:    "tom",
+					ExpensesSum: 60.0,
+					Salary:      900,
+					Owes:        0,
+				},
+			},
+			wantErr: false,
+			updatedArgs: map[string]float64{
+				"alice": 12.6315,
+				"tom":   0.0,
+			},
+		},
+		{
+			name: "Person with greater salary has more purchases",
+			args: args{
+				ctx: nil,
+				user1: &db.StatisticsAggrByTimespanRow{
+					Username:    "alice",
+					ExpensesSum: 100.0,
+					Salary:      1000.0,
+					Owes:        0,
+				},
+				user2: &db.StatisticsAggrByTimespanRow{
+					Username:    "tom",
+					ExpensesSum: 20.0,
+					Salary:      700,
+					Owes:        0,
+				},
+			},
+			wantErr: false,
+			updatedArgs: map[string]float64{
+				"alice": 0.0,
+				"tom":   29.4117,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := GetSalaryCompensatedDebts(tt.args.ctx, tt.args.user1, tt.args.user2)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetSalaryCompensatedDebts() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if math.Abs(tt.updatedArgs[tt.args.user1.Username]-tt.args.user1.Owes) > floatDelta {
+				t.Errorf("%s: %s: %.4f != %.4f",
+					tt.name,
+					tt.args.user1.Username,
+					tt.updatedArgs[tt.args.user1.Username],
+					tt.args.user1.Owes)
+			}
+
+			if math.Abs(tt.updatedArgs[tt.args.user2.Username]-tt.args.user2.Owes) > floatDelta {
+				t.Errorf("%s: %s: %.4f != %.4f",
+					tt.name,
+					tt.args.user2.Username,
+					tt.updatedArgs[tt.args.user2.Username],
+					tt.args.user2.Owes)
+			}
+		})
+	}
+}
