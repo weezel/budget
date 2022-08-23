643228b9f73ca5759e41d66da61b460c5e5b41a9
Ville Valkonen <weezelding@gmail.com>
2022-08-23 12:19:29 +0300

    Simplify debt calculations.

--- debtcontrol/debtcontrol.go
+++ debtcontrol/debtcontrol.go
@@ -33,33 +33,26 @@ func GetSalaryCompensatedDebts(
 	lowerIncomeRatio := debts[0].Salary / sumSalaries
 	greaterIncomeRatio := debts[1].Salary / sumSalaries
 
-	lowerIncomeOwes := debts[1].ExpensesSum * lowerIncomeRatio
-	greaterIncomeOwes := debts[0].ExpensesSum * greaterIncomeRatio
+	debts[1].Owes = debts[1].ExpensesSum * lowerIncomeRatio
+	debts[0].Owes = debts[0].ExpensesSum * greaterIncomeRatio
 
-	totalExpenses := float64(debts[0].ExpensesSum + debts[1].ExpensesSum)
-	expRatioByLowerInc := debts[0].ExpensesSum / totalExpenses
-	expRatioByGreaterInc := debts[1].ExpensesSum / totalExpenses
+	debt := math.Abs(debts[0].Owes - debts[1].Owes)
 
-	debt := math.Abs(greaterIncomeOwes - lowerIncomeOwes)
-
+	logger.Debugf("Debts fetched: %#v", debts)
 	logger.Debugf("Sum of salaries: %.2f", sumSalaries)
 	logger.Debugf("Lower income ration: %.2f", lowerIncomeRatio)
-	logger.Debugf("Lower income owes: %.2f", lowerIncomeOwes)
+	logger.Debugf("Lower income owes: %.2f", debts[0].Owes)
 	logger.Debugf("Greater income ration: %.2f", greaterIncomeRatio)
-	logger.Debugf("Greater income owes: %.2f", greaterIncomeOwes)
-	logger.Debugf("Expenses ratio by lower income: %.2f", expRatioByLowerInc)
-	logger.Debugf("Expenses ratio by greater income: %.2f", expRatioByGreaterInc)
-	logger.Debugf("Total expenses: %.2f", totalExpenses)
+	logger.Debugf("Greater income owes: %.2f", debts[1].Owes)
 	logger.Debugf("Debt in the end: %.2f", debt)
 
-	if expRatioByLowerInc < expRatioByGreaterInc {
+	if debts[0].Owes < debts[1].Owes {
 		debts[0].Owes = debt
 		debts[1].Owes = 0.0
 	} else {
 		debts[0].Owes = 0.0
 		debts[1].Owes = debt
 	}
-	logger.Debugf("Debts fetched: %#v", debts)
 
 	return nil
 }
